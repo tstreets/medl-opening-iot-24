8f72ea1aabde91d0b6ec8669451ffa6c69223dc2
Ty Streets <tstreets@iu.edu>
2024-11-15 09:35:26 -0500

    Added sockets for creature creation

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
 app.use(cors());
@@ -15,5 +17,41 @@ app.use("/api/creature-combat", require("./creature-combat"));
 app.all("/api/*", function (req, res) {
   res.status(404).json({ error: "Endpoint not found" });
 });
+
+app.use(express.static(path.join(__dirname, "../views")));
+
+app.use("socket.io", express.static("node_modules/socket.io"));
+
+const port = process.env.PORT || 3007;
+
+const server = app.listen(port);
+
+const io = new Server(server);
+
+const users = {};
+
+io.on("connection", (socket) => {
+  const clientId = socket.client.id;
+  const timeJoined = Date.now();
+  users[clientId] = { timeJoined, creature: {} };
+
+  socket.on(
+    "creature-attempt-ready",
+    function ({ creatureNum, includeImages }) {
+      users[clientId].ready = true;
+      if (creatureNum === 2 || creatureNum === 1) {
+        users[clientId].creature.creatureNum = creatureNum;
+        users[clientId].creature.includeImages = includeImages;
+        users[clientId].email = userEmail;
+      }
+    }
+  );
+
+  socket.on("disconnect", function () {
+    // delete users[clientId];
+  });
+});
+
+console.log(`Local server running at http://localhost:${port}`);
 
 module.exports = app;
--- views/creature-combat/battle.js
+++ views/creature-combat/battle.js
@@ -1,3 +1,22 @@
+import "/socket.io/socket.io.js";
+
+const socket = io();
+
+document.querySelector("#form-confirm").onclick = function () {
+  const userEmail = document.querySelector("#email").value.trim().toLowerCase();
+  const includeImages = document.querySelector("#pics").checked;
+
+  if (userEmail) {
+    const creatureNum = location.hash.split("#p")[1].slice(0, 1);
+    socket.emit("creature-attempt-ready", {
+      creatureNum: Number(creatureNum),
+      includeImages: includeImages,
+      userEmail: userEmail,
+    });
+  }
+};
+
+document.querySelector('a[href="#surrender"]').style.display = "none";
 document.querySelector('a[href="#surrender"]').onclick = function (e) {
   e.preventDefault();
   location.assign("/creature-combat");
